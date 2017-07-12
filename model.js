c38c1a980998046425d9c08ecc07bcd0f55dde0c
Elush Shirazpour <eshirazp@gmail.com>
2017-07-11 20:26:43 -0700

    Added support for Mocha and Chai for the Mongo db

--- model.js
+++ model.js
@@ -1,30 +1,30 @@
-"use strict";
-
 const mongoose = require('mongoose');
 
-const BlogPostSchema = mongoose.Schema({
-  "title":    {type: String, required: true},
-  "content":  {type: String, required: true},
-  "author": 
-  {
-    "firstName":  {type: String, required: true},
-    "lastName":   {type: String, required: true}
+const blogPostSchema = mongoose.Schema({
+  author: {
+    firstName: String,
+    lastName: String
   },
-  "created": {type: Date, default: Date.now}
+  title: {type: String, required: true},
+  content: {type: String},
+  created: {type: Date, default: Date.now}
 });
 
-BlogPostSchema.virtual('authorName').get(function() {
-  return `${this.author.firstName} ${this.author.lastName}`.trim();
-})
 
-BlogPostSchema.methods.apiRepr = function() {
+blogPostSchema.virtual('authorName').get(function() {
+  return `${this.author.firstName} ${this.author.lastName}`.trim();
+});
+
+blogPostSchema.methods.apiRepr = function() {
   return {
-    title: this.title,
-    content: this.content,
+    id: this._id,
     author: this.authorName,
+    content: this.content,
+    title: this.title,
     created: this.created
   };
 }
 
-const BlogPost = mongoose.model('BlogPost', BlogPostSchema);
+const BlogPost = mongoose.model('BlogPost', blogPostSchema);
+
 module.exports = {BlogPost};
